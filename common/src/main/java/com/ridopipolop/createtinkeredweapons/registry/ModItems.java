1fd135d75b2e3b42a1d60594df64205ef029ef4e
GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>
2026-03-31 23:42:18 +0200

    Impact Axe implemented
    Ported over the `Impact Axe` from my fabric project

--- common/src/main/java/com/ridopipolop/createtinkeredweapons/registry/ModItems.java
+++ common/src/main/java/com/ridopipolop/createtinkeredweapons/registry/ModItems.java
@@ -1,8 +1,19 @@
 package com.ridopipolop.createtinkeredweapons.registry;
 
+import static com.ridopipolop.createtinkeredweapons.CreateTinkeredWeapons.REGISTRATE;
+
 import com.ridopipolop.createtinkeredweapons.CreateTinkeredWeapons;
+import com.ridopipolop.createtinkeredweapons.content.weapons.impact_axe.ImpactAxeItem;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.tterrag.registrate.util.entry.ItemEntry;
 
 public class ModItems {
+  public static final ItemEntry<ImpactAxeItem> IMPACT_AXE = REGISTRATE
+    .item("impact_axe", ImpactAxeItem:: new)
+    .properties(p -> p.stacksTo(1).durability(350))
+    .model(AssetLookup.itemModelWithPartials())
+    .register();
+
 
   public static void register() {
     CreateTinkeredWeapons.LOGGER.info("Registiring items for {}.", CreateTinkeredWeapons.NAME);
