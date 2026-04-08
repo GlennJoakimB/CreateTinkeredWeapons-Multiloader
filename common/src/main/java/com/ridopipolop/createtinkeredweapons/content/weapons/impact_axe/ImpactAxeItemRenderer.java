ec9df6d6a7592e6318618f7a325e067642407f5e
Glenn Joakim B <89195051+GlennJoakimB@users.noreply.github.com>
2026-04-08 16:40:24 +0200

    Feature/model updates (#4)
    Updates to models and textures
    - Updated textures for both BroadGlaive and ImpactAxe
    - Adjusted colors to match closer to similar Brass-items in Create
    - Adjusted placements and some model-faces
    - Added explosive-mode textures for ImpactAxe
    - Adjusted Impact-axe renderer to speed up gear rotation
    
    Also added crafting recipes

--- common/src/main/java/com/ridopipolop/createtinkeredweapons/content/weapons/impact_axe/ImpactAxeItemRenderer.java
+++ common/src/main/java/com/ridopipolop/createtinkeredweapons/content/weapons/impact_axe/ImpactAxeItemRenderer.java
@@ -11,19 +11,34 @@ import com.simibubi.create.foundation.item.render.PartialItemModelRenderer;
 import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 import net.createmod.catnip.animation.AnimationTickHolder;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 public class ImpactAxeItemRenderer extends CustomRenderedItemModelRenderer {
 
   protected static final PartialModel GEAR = PartialModel.of(CreateTinkeredWeapons.id("item/impact_axe/gear"));
+  protected static final PartialModel EXPLOSIVE_MODEL = PartialModel
+      .of(CreateTinkeredWeapons.id("item/impact_axe/item_explosive"));
 
   @Override
   protected void render(ItemStack stack, CustomRenderedItemModel model, PartialItemModelRenderer renderer,
       ItemDisplayContext transformType, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
 
-    renderer.render(model.getOriginalModel(), light);
-    ms.mulPose(Axis.YP.rotationDegrees(ScrollValueHandler.getScroll(AnimationTickHolder.getPartialTicks())));
+    boolean isExplosiveMode = ImpactAxeItem.isExplosiveMode(stack);
+    float worldTime = AnimationTickHolder.getRenderTime() / 10;
+    float angle = worldTime * (isExplosiveMode ? 30 : 20);
+    BakedModel baseModel = isExplosiveMode
+        ? EXPLOSIVE_MODEL.get()
+        : model.getOriginalModel();
+
+    // Main model
+    renderer.render(baseModel, light);
+
+    // Cog animation
+    ms.pushPose();
+    ms.mulPose(Axis.YP.rotationDegrees(angle));
     renderer.render(GEAR.get(), light);
+    ms.popPose();
   }
 }
