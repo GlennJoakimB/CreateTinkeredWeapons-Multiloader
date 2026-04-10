ef3d1dee8805c66948b85f1e7bdd949695937a4c
GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>
2026-04-10 21:37:37 +0200

    Redundant attributes
    Removed redundant attributes for attackdamage and speed on the ImpactAxe

--- common/src/main/java/com/ridopipolop/createtinkeredweapons/content/weapons/impact_axe/ImpactAxeItem.java
+++ common/src/main/java/com/ridopipolop/createtinkeredweapons/content/weapons/impact_axe/ImpactAxeItem.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Multimap;
-
 import com.simibubi.create.content.equipment.armor.BacktankUtil;
 
 import net.minecraft.ChatFormatting;
@@ -22,10 +19,6 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tiers;
@@ -43,16 +36,9 @@ public class ImpactAxeItem extends AxeItem {
   public static final int BASE_DURABILITY = 250;
   private static final String EXPLOSIVE_MODE_KEY = "ExplosiveMode";
   private static final double KNOCKBACK_STRENGTH = 0.2;
-  private final Multimap<Attribute, AttributeModifier> defaultModifiers;
 
   public ImpactAxeItem(Properties properties) {
     super(Tiers.IRON, (int) BASE_DAMAGE, BASE_ATTACK_SPEED, properties);
-    ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-    builder.put(Attributes.ATTACK_DAMAGE,
-        new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", BASE_DAMAGE, Operation.ADDITION));
-    builder.put(Attributes.ATTACK_SPEED,
-        new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", BASE_ATTACK_SPEED, Operation.ADDITION));
-    this.defaultModifiers = builder.build();
   }
 
   @Override
@@ -306,11 +292,6 @@ public class ImpactAxeItem extends AxeItem {
     } else {
       return BacktankUtil.getBarColor(stack, BASE_DURABILITY);
     }
-  }
-
-  @Override
-  public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot slot) {
-    return slot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(slot);
   }
 
   @Override
