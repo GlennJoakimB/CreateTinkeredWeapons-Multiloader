0e971faf4bc7def3df2677021714aa23f61af557
GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>
2026-04-25 19:06:10 +0200

    Rendering rework
    - Reworked `PropellerMaceItemRenderer`
      - Refactored some parts into private methods
      - Added transforms in code, in place of using display-values from model.json

--- common/src/main/java/com/ridopipolop/createtinkeredweapons/content/weapons/propeller_mace/PropellerMaceItemRenderer.java
+++ common/src/main/java/com/ridopipolop/createtinkeredweapons/content/weapons/propeller_mace/PropellerMaceItemRenderer.java
@@ -26,24 +26,54 @@ public class PropellerMaceItemRenderer extends CustomRenderedItemModelRenderer {
   protected void render(ItemStack stack, CustomRenderedItemModel model, PartialItemModelRenderer renderer,
       ItemDisplayContext transformType, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
 
+    // If not deployed, render original model
     if (!PropellerMaceItem.isDeployedMode(stack)) {
       renderer.render(model.getOriginalModel(), light);
-    } else {
-      renderer.render(DEPLOYED_MODEL.get(), light);
-
-      float worldTime = AnimationTickHolder.getRenderTime() / 10;
-      float angle = worldTime * 600;
-
-      // Rotor animations
-      ms.pushPose();
-      ms.mulPose(Axis.YP.rotationDegrees(angle)); // Rotate one way
-      renderer.render(PROPELLER_1.get(), light);
-      ms.popPose();
-
-      ms.pushPose();
-      ms.mulPose(Axis.YP.rotationDegrees(-angle)); // Rotate the other way
-      renderer.render(PROPELLER_2.get(), light);
-      ms.popPose();
+      return;
     }
+
+    ms.pushPose();
+
+    // 1. Apply manual offsets based on transformType
+    applyDeploymentTransform(transformType, ms);
+
+    // 2. Render the body
+    renderer.render(DEPLOYED_MODEL.get(), light);
+
+    // 3. Render animated propeller
+    float angle = AnimationTickHolder.getRenderTime() * 60;
+    renderRotatingPropeller(renderer, ms, PROPELLER_1, angle, light);
+    renderRotatingPropeller(renderer, ms, PROPELLER_2, -angle, light);
+
+    ms.popPose();
+  }
+
+  private void applyDeploymentTransform(ItemDisplayContext context, PoseStack ms) {
+    if (context.firstPerson()) {
+      boolean isLeftHand = context == ItemDisplayContext.FIRST_PERSON_LEFT_HAND
+          || context == ItemDisplayContext.THIRD_PERSON_LEFT_HAND;
+      float flip = isLeftHand ? -1f : 1f;
+
+      // Apply Translation (X flipped for left hand)
+      ms.translate((0.25f * flip) / 16f, 5.5f / 16f, 0);
+
+      // Apply Rotation (Y and Z flipped for left hand)
+      ms.mulPose(Axis.XP.rotationDegrees(-1.75f));
+      ms.mulPose(Axis.YP.rotationDegrees(-0.25f * flip));
+      ms.mulPose(Axis.ZP.rotationDegrees(-16f * flip));
+
+    } else if (context == ItemDisplayContext.THIRD_PERSON_RIGHT_HAND
+        || context == ItemDisplayContext.THIRD_PERSON_LEFT_HAND) {
+      ms.translate(0, -4.0f / 16f, -4.0f / 16f);
+      ms.mulPose(Axis.XP.rotationDegrees(-82f));
+    }
+  }
+
+  private void renderRotatingPropeller(PartialItemModelRenderer renderer, PoseStack ms, PartialModel part, float angle,
+      int light) {
+    ms.pushPose();
+    ms.mulPose(Axis.YP.rotationDegrees(angle));
+    renderer.render(part.get(), light);
+    ms.popPose();
   }
 }
